a0b75e9ec6447f48a9b5b32b05aebe44eeecdd70
KTPx4 <px4.vnd@gmail.com>
2023-04-05 19:56:44 +0700

    small update

--- app/views/views.php
+++ app/views/views.php
@@ -15,7 +15,7 @@
     <a href="#"><img src="logo.png" alt="Logo"></a>
   </div>
   <div class="menu">
-    <a href="#">TRANG CHỦ</a>
+    <a href="/home">TRANG CHỦ</a>
     <a href="#">Giới thiệu</a>
     <div class="dropdown">
       <a href="#">Dịch vụ</a>
@@ -32,8 +32,23 @@
     <span></span>
     <span></span>
   </div>
+  
+</div>
+<div class="content">
+    <?php 
+        require_once  $detail ;
+    ?>
 </div>
 
+<div class="footer">
+  <ul>
+    <li class="page"><a href="">1</a></li>
+    <li class="page"><a href="">2</a></li>
+    <li class="page"><a href="">3</a></li>
+    <li class="page"><a href="">4</a></li>
+    <li class="page"><a href="">5</a></li>
+  </ul>
+</div>
 
 </body>
 
