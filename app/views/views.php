c2ff9f114cc4d571f9c7b470b55b855cbda2574d
KTPx4 <px4.vnd@gmail.com>
2023-04-05 15:10:42 +0700

    test branch

--- app/views/views.php
+++ app/views/views.php
@@ -10,23 +10,35 @@
 </head>
 <body>
     
-    
-<nav>
-    <div class="navbar">
-            <div class="navbar-inner">
-                <ul>
-                    <li><a href="#">TRANG CHỦ</a></li>
-                    <li><a href="#">DẠNG ANIME</a></li>
-                    <li><a href="#">TOP ANIME</a></li>
-                    <li><a href="#">THỂ LOẠI</a></li>
-                    <li><a href="#">SEASON</a></li>
-                    <li><a href="#">THƯ VIỆN</a></li>
-                    <li><a href="#">LỊCH CHIẾU</a></li>
-                </ul>
-            </div>
-    </div>
-    
-</nav>
+    <nav>
+        <div class="navbar">
+                <div class="navbar-inner">
+                    <ul>
+                        <li><a href="#">TRANG CHỦ</a></li>
+                        <li><a href="#">DẠNG ANIME</a></li>
+                        <li><a href="#">TOP ANIME</a></li>
+                        <li><a href="#">THỂ LOẠI</a></li>
+                        <li><a href="#">SEASON</a></li>
+                        <li><a href="#">THƯ VIỆN</a></li>
+                        <li><a href="#">LỊCH CHIẾU</a></li>
+                    </ul>
+                </div>
+        </div>
 
+    </nav>
+    <div class="content">
+        <p>text content</p>
+    </div>
+
+    <div class="page"> 
+        <ul>
+            <li class="num-pages"><a href="">1</a></li>
+            <li class="num-pages"><a href="">2</a></li>
+            <li class="num-pages"><a href="">3</a></li>
+            <li class="num-pages"><a href="">4</a></li>
+        </ul>
+    </div>
+    <div class="footer"> </div>
+</body>
 
 </html>
