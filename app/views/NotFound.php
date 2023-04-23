765126b4df0aaf46ca209bf94f6648d894e517b0
CalendarIT <128311636+khoaDevITL2702@users.noreply.github.com>
2023-04-23 22:14:48 +0700

    edit small

--- app/views/NotFound.php
+++ app/views/NotFound.php
@@ -1,18 +1,38 @@
-<section class="page_404">
-    <div class="container">
-        <div class="row">
-        <div class="col-sm-12 ">
-        <div class="col-sm-10 col-sm-offset-1 text-center">
-        <div class="four_zero_four_bg">
-            <h1 class="text-center">404</h1>
+<?php
+  $root_img='http://'.$_SERVER['HTTP_HOST'] . "/public/img/";
+  $HOST = "http://".$_SERVER['HTTP_HOST'];
+  define('HOST', $HOST);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 NOT FOUNDs</title>
+    <link rel="stylesheet" href="<?=HOST?>/public/css/cssElm.css" type="text/css">
+
+</head>
+<body>
+    
+    <section class="page_404">
+        <div class="container">
+            <div class="row">
+            <div class="col-sm-12 ">
+            <div class="col-sm-10 col-sm-offset-1 text-center">
+            <div class="four_zero_four_bg">
+                <h1 class="text-center">404</h1>
+            </div>
+            <div clasa="contant_box_404">
+            <h3 class="h2">Look like you're lost</h3>
+            <p>The page you are looking fot not avaible!</p>
+            <a href="<?=HOST?>" class="link_404">Go to Home</a>
         </div>
-        <div clasa="contant_box_404">
-        <h3 class="h2">Look like you're lost</h3>
-        <p>The page you are looking fot not avaible!</p>
-        <a href="" class="link_404">Go to Home</a>
-    </div>
+            </div>
+            </div>
+            </div>
         </div>
-        </div>
-        </div>
-    </div>
-</section>
+    </section>
+
+</body>
+</html>
